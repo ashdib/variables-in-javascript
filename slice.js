ba3ecc18e8b00216c453c27caba774a9d2e5a328
axeladib <nabil298adib@gmail.com>
2024-01-15 02:24:46 +0800

    slice the text that exceeds the  limitation

---
+++ slice.js
@@ -0,0 +1,31 @@
+//slice built-in function : to separate the characters in string according to x and y values.
+// syntax : string.slice(x,y);
+
+// create a prompt for input of string
+var inputText = prompt("Compose text here");
+
+//get length of the text
+var getLengthText = inputText.length;
+
+// limit 140 characters
+var limitcharacters = 140;
+
+// statement
+// You have written 182 characters and -42 is remaining
+
+alert(
+  "You have written" +
+    " " +
+    getLengthText +
+    " characters" +
+    " " +
+    "and " +
+    (limitcharacters - getLengthText) +
+    " is remaining"
+);
+
+// get the text the exceeds the limit of characters
+
+var getTextExceeds = inputText.slice(140, getLengthText);
+
+alert("This is the text that exceeds 140 characters" + " : " + getTextExceeds);
