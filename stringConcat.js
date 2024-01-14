e70237f7ec82f871ec3532a07e4720f5423085ee
axeladib <nabil298adib@gmail.com>
2024-01-15 01:44:11 +0800

    string concatenation

---
+++ stringConcat.js
@@ -0,0 +1,14 @@
+var yourName = prompt("What is your spoilt name?");
+
+var messageBeautiful = "Why you look so beautiful tonight?" ;
+
+var messageCompliment = "You know you look gorgeous when you are doing your works";
+
+var messageLoving = "Just love me only okay?";
+
+var messageFinal = "I will and always love my boyfriend Adib only" + " " + "-" + yourName + "-"
+
+alert(messageBeautiful);
+alert(messageCompliment);
+alert(messageLoving);
+alert(messageFinal);
