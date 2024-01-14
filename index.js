b64990d95a011ccb8d611b340e20bf8a4e55fe3d
axeladib <nabil298adib@gmail.com>
2024-01-15 01:21:54 +0800

    exercise

---
+++ index.js
@@ -0,0 +1,18 @@
+// can't redeclare the variables a and b
+// Can't insert numbers
+
+var a = "3";
+var b = "8";
+
+/***********Do not change the code above 👆*******/
+//Write your code on lines 7 - 9:
+var c = a; // 3
+a = b; // 8
+b = c // 3
+
+
+
+/***********Do not change the code below 👇*******/
+
+console.log("a is " + a);
+console.log("b is " + b);
