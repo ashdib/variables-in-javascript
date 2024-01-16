8d512db634f4383347d681e6a39af282475b879a
axeladib <nabil298adib@gmail.com>
2024-01-16 16:15:48 +0800

    dogAge Calculator

---
+++ arithmeticChallenge.js
@@ -0,0 +1,14 @@
+ // Cat Age to Human Age Formula
+
+ // Formula humanAge = (dogAge -2) x 4+ 21
+
+ // This challenge test your knowledge on how airithmetic operation in javascript works
+
+// ask the dog age first
+var dogAge = prompt("How old your dog?");
+
+//then from variable store dogAge perform operation to convert to human age
+humanAge = ((dogAge - 2) * 4) + 21;
+
+//Alert the user the result of the dogAge according to humanAge
+alert("Your dog age according to current human age is " + humanAge);
