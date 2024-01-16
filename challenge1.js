95d36124eb7ad89450a068d96930786411a5cf0e
axeladib <nabil298adib@gmail.com>
2024-01-16 16:04:52 +0800

    add restofChar

--- challenge1.js
+++ challenge1.js
@@ -1,7 +1,13 @@
 // Challenge : Change the first letter to capital letter and return new statement with first letter is capital letter.
 
+//TODO: Get the user name
 var userName = prompt("What is your name");
 
+//TODO: Change the first letter of from the userName to capital
 var firstCapitalChar = userName.slice(0,1).toUpperCase();
 
-alert(firstCapitalChar + userName.slice(1,userName.length));
+//TODO: Make sure the rest of character after the first capital is lower capital
+var restOfChar = userName.slice(1,userName.length).toLowerCase();
+
+//TODO; Concatenate the userName and greeting the user
+alert("Hello welcome to WriteFast, "+firstCapitalChar + restOfChar);
